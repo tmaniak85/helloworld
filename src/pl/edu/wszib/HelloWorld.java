0334c83df3df5a33e21193347591cf1e2a880f63
tmaniak85 <tomasz.maniak.85>
2020-12-12 09:47:57 +0100

    Propozycja zmian

--- src/pl/edu/wszib/HelloWorld.java
+++ src/pl/edu/wszib/HelloWorld.java
@@ -2,6 +2,9 @@ package pl.edu.wszib;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World!");
+        }
+        System.out.println("Koniec programu");
     }
 }
